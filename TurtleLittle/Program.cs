c1a7e88e0421fb9aa6bda810adfd89613ddebd99
Sara894 <t48250klo@gmail.com>
2022-03-09 12:11:26 +0300

    Добавьте файлы проекта.

---
+++ TurtleLittle/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
+
+namespace TurtleLittle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
