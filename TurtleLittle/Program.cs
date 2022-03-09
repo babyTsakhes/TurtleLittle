815a215a637a56466136ffa7b5492a1d51a317ea
Sara894 <t48250klo@gmail.com>
2022-03-09 19:54:19 +0300

    еда бегает за черепахой

--- TurtleLittle/Program.cs
+++ TurtleLittle/Program.cs
@@ -13,13 +13,21 @@ namespace TurtleLittle
             Shapes.Move(eat, 200, 200);*/
             Turtle.X = 100;
             Turtle.Y = 100;
-            while (true)
-            {
-                Turtle.Move(10);
-                for (int i = 0; i < 7; i++)
-                {
-                    AddFood(200 - i * 7, 200 - i * i * (i + 1) * 15, 10 + i * 2, 10 - i, "Red");
-                }
+            
+              while (true)
+              {
+                  Turtle.Move(10);
+                GraphicsWindow.BrushColor = "Red";
+                var eat = Shapes.AddRectangle(20, 20);
+                Shapes.Move(eat, 200, 300);
+              /*  if (20 == Turtle.X && 30 == Turtle.Y)*/
+                    Shapes.Move(eat, 30, 40);
+                    Shapes.Move(eat, Turtle.X, Turtle.Y+10);
+                /* for (int i = 0; i < 7; i++)
+                 {
+
+                     //AddFood(200 - i * 7, 200 - i * i * (i + 1) * 15, 10 + i * 2, 10 - i, "Red");
+                 }*/
             }
         }
 
