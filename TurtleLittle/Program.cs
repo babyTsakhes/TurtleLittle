5904dc7aa50113ad4275e4307dee66e8fb25a836
Sara894 <t48250klo@gmail.com>
2022-03-09 17:38:45 +0300

    M

--- TurtleLittle/Program.cs
+++ TurtleLittle/Program.cs
@@ -40,31 +40,48 @@ namespace TurtleLittle
             Turtle.Angle = 180;
             Turtle.Move(size);
         }
+
+        static void WriteM(int size)
+        {
+            Turtle.Angle = 0;
+            Turtle.Move(size);
+            Turtle.MoveTo(Turtle.X+size/2, Turtle.Y);
+            Turtle.MoveTo(Turtle.X, Turtle.Y+size/2);
+            Turtle.MoveTo(Turtle.X + size/4, Turtle.Y);
+            Turtle.MoveTo(Turtle.X , Turtle.Y - size/2);
+            Turtle.MoveTo(Turtle.X + size/2, Turtle.Y);
+            Turtle.MoveTo(Turtle.X, Turtle.Y + size);
+
+
+        }
         static void Main(string[] args)
         {
-            Turtle.Speed = 8;
-            //coordinates
+            /*  Turtle.Speed = 8;
+              //coordinates
+              Turtle.X = 200;
+              Turtle.Y = 200;
+
+              //T
+              WriteT(40);
+
+              Turtle.X = 260;
+              Turtle.Y = 200;
+              WriteO();
+
+              Turtle.X = 360;
+              Turtle.Y = 180;
+              WriteR(40);
+
+
+
+              //T
+              Turtle.X = 440;
+              Turtle.Y = 200;
+              WriteT(20);*/
             Turtle.X = 200;
             Turtle.Y = 200;
-
-            //T
-            WriteT(20);
-
-            Turtle.X = 260;
-            Turtle.Y = 200;
-            WriteO();
-
-            Turtle.X = 360;
-            Turtle.Y = 180;
-            WriteR(40);
-
-
-
-            //T
-            Turtle.X = 440;
-            Turtle.Y = 200;
-            WriteT(20);
-
+            WriteM(50);
+            Turtle.Hide();
         }
     }
 }
