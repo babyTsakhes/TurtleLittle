759e37ae2017b350e278f47c2c0fa2cfe5f00126
Sara894 <t48250klo@gmail.com>
2022-03-09 12:33:25 +0300

    пишем слово ТОРТ

--- TurtleLittle/Program.cs
+++ TurtleLittle/Program.cs
@@ -9,12 +9,31 @@ namespace TurtleLittle
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
             Turtle.Speed = 8;
             //coordinates
             Turtle.X = 200;
             Turtle.Y = 200;
+
+            //T
+            Turtle.Angle = 0;
+            Turtle.Move(60);
+            Turtle.TurnLeft();
+            Turtle.Move(20);
+            Turtle.Angle = 90;
+            Turtle.Move(40);
+
+            Turtle.X = 260;
+            Turtle.Y = 200;
+            Turtle.Angle = 0;
+
+            for(int i = 0; i < 4; i++)
+            {
+                Turtle.Move(60);
+                Turtle.TurnRight();
+            }
 
             //T
             Turtle.Angle = 0;
