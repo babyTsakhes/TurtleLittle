be1cea233293ebfb58f12469b78e4f6a01947e1a
Sara894 <t48250klo@gmail.com>
2022-03-09 14:01:02 +0300

    тот

--- TurtleLittle/Program.cs
+++ TurtleLittle/Program.cs
@@ -9,7 +9,25 @@ namespace TurtleLittle
 {
     class Program
     {
-       
+        static void WriteT()
+        {
+            Turtle.Angle = 0;
+            Turtle.Move(60);
+            Turtle.TurnLeft();
+            Turtle.Move(20);
+            Turtle.Angle = 90;
+            Turtle.Move(40);
+        }
+
+        static void WriteO()
+        {
+            Turtle.Angle = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                Turtle.Move(60);
+                Turtle.TurnRight();
+            }
+        }
         static void Main(string[] args)
         {
             Turtle.Speed = 8;
@@ -18,30 +36,18 @@ namespace TurtleLittle
             Turtle.Y = 200;
 
             //T
-            Turtle.Angle = 0;
-            Turtle.Move(60);
-            Turtle.TurnLeft();
-            Turtle.Move(20);
-            Turtle.Angle = 90;
-            Turtle.Move(40);
+            WriteT();
 
             Turtle.X = 260;
             Turtle.Y = 200;
-            Turtle.Angle = 0;
+            WriteO();
 
-            for(int i = 0; i < 4; i++)
-            {
-                Turtle.Move(60);
-                Turtle.TurnRight();
-            }
+
 
             //T
-            Turtle.Angle = 0;
-            Turtle.Move(60);
-            Turtle.TurnLeft();
-            Turtle.Move(20);
-            Turtle.Angle = 90;
-            Turtle.Move(40);
+            Turtle.X = 400;
+            Turtle.Y = 200;
+            WriteT();
 
         }
     }
